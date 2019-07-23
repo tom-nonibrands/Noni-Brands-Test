843b117b66976cb9952701b9fff0857e646ce48c
TOm <tom@Nonibrands.com>
2019-07-23 10:43:17 -0400

    inital

---
+++ main.js
@@ -0,0 +1,17 @@
+ /* 
+    INSTRUCTIONS
+
+    PART 2: Javascript
+    
+    When clicking on your newly styled button, connect 
+    to an API using an GET ajax request. Spend 
+    approximately 25 minutes on it.
+    
+    API Endpoint: https://newsapi.org/v2/top-headlines?country=us&apiKey=9e56665dd12747d699700625112fe619
+    
+    Display the new articles you get from the API in a visually appealing way.
+    
+*/
+ $(function () {
+
+ });
